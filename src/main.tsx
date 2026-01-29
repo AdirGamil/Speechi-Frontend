3eaf3fa7c3ad7ea224c7a4311db3a1193bf89a24
AdirGamil <2000adir2000@gmail.com>
2026-01-29 21:22:59 +0200

    Fix strict

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 import "./app/styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  // <StrictMode>
+  <StrictMode>
     <App />
-  // </StrictMode>
+  </StrictMode>
 );
