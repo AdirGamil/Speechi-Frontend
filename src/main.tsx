1b26326cb3b8b54802c47317301e75f6c210b5ab
AdirGamil <2000adir2000@gmail.com>
2026-01-29 21:20:51 +0200

    update dropdown ui

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./app/lib/gsap";
 import App from "./App";
 import "./index.css";
+import "./app/styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  // <StrictMode>
     <App />
-  </StrictMode>
+  // </StrictMode>
 );
